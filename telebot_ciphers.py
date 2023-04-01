052679b636adc9af7eb0eeff17880e8df28b20cf
dantol29 <83882235+dantol29@users.noreply.github.com>
2023-04-01 10:55:09 +0100

    Ceaser cipher, code word cipher, Atbash cipher, Playfair cipher,  almost XOR

--- telebot_ciphers.py
+++ telebot_ciphers.py
@@ -2,7 +2,7 @@ import telebot
 from telebot import types
 import requests
 
-bot = telebot.TeleBot('5459758083:AAGHeM1TaQleUziGXF6umrSe-VdaIckuC4s')
+bot = telebot.TeleBot('')
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '/']
 alphabet_2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '/']
 opposite = ['z', 'y', 'x', 'w', 'v', 'u', 't', 's', 'r', 'q', 'p', 'o', 'n', 'm', 'l', 'k', 'j', 'i', 'h', 'g', 'f', 'e', 'd', 'c', 'b', 'a', '/']
